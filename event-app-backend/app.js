d53c5b0387583a6f124a848f0dc2646cca681e30
Caitlyn Myland <caitlyn.myland@gmail.com>
2024-10-20 12:55:55 -0400

    Use env variable PORT for server start

--- event-app-backend/app.js
+++ event-app-backend/app.js
@@ -10,7 +10,7 @@ import { dbConfig } from './config.js';
 const { Pool } = pkg; // Using Pool for PostgreSQL
 
 const app = express();
-const PORT = 3001; //process.env.PORT
+const PORT = process.env.PORT;
 
 app.listen(PORT, (err) => {
     if (err) {
